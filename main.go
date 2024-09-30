574549663fc5be3722865dee087d16a7cfe1417d
Howard Lim <holim@moneylion.com>
2024-09-30 10:48:24 +0800

    feat: github branch protection rules testing

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
@@ -11,10 +12,16 @@ import (
 	kiotaSerialization "github.com/microsoft/kiota-abstractions-go/serialization"
 	kiotaHttp "github.com/microsoft/kiota-http-go"
 	"github.com/octokit/go-sdk/pkg"
+	"github.com/octokit/go-sdk/pkg/github/models"
 
 	"bytes"
 	"io"
 	"net/http"
+	"reflect"
+)
+
+const (
+	BranchNotProtectedMessage = "Branch not protected"
 )
 
 func printRequest(req *http.Request) {
@@ -47,6 +54,65 @@ func printRequest(req *http.Request) {
 
 type LogHandler struct{}
 
+func printErrorProperties(err any) {
+	// Use reflection to inspect the error's properties
+	val := reflect.ValueOf(err)
+	typ := reflect.TypeOf(err)
+
+	if typ.Kind() == reflect.Ptr {
+		// Dereference the pointer to get the underlying type
+		val = val.Elem()
+		typ = typ.Elem()
+	}
+
+	// Print the type and its fields
+	log.Printf("Type: %s\n", typ.Name())
+	for i := 0; i < typ.NumField(); i++ {
+		field := typ.Field(i)
+		value := val.Field(i)
+
+		// Check if the field is exported (public)
+		if field.PkgPath == "" {
+			// Exported fields have an empty PkgPath
+			log.Printf("%s: %v\n", field.Name, value.Interface())
+		} else {
+			// Unexported fields are skipped
+			log.Printf("%s: (unexported)\n", field.Name)
+		}
+	}
+}
+
+// func printStructFields(s any, indent string) {
+// 	val := reflect.ValueOf(s)
+// 	typ := reflect.TypeOf(s)
+
+// 	// If it's a pointer, dereference it
+// 	if typ.Kind() == reflect.Ptr {
+// 		val = val.Elem()
+// 		typ = typ.Elem()
+// 	}
+
+// 	// Iterate through the struct's fields
+// 	for i := 0; i < typ.NumField(); i++ {
+// 		field := typ.Field(i)
+// 		value := val.Field(i)
+
+// 		// Check if the field is exported (public)
+// 		if field.PkgPath == "" {
+// 			// If the field is a struct, recursively print its fields
+// 			if value.Kind() == reflect.Struct {
+// 				log.Printf("%s%s (struct):\n", indent, field.Name)
+// 				printStructFields(value.Interface(), indent+"  ") // Recursive call with increased indent
+// 			} else {
+// 				log.Printf("%s%s: %v\n", indent, field.Name, value.Interface())
+// 			}
+// 		} else {
+// 			// Unexported field, cannot be accessed
+// 			log.Printf("%s%s: (unexported)\n", indent, field.Name)
+// 		}
+// 	}
+// }
+
 func (handler LogHandler) Intercept(pipeline kiotaHttp.Pipeline, middlewareIndex int, request *http.Request) (*http.Response, error) {
 	printRequest(request)
 	log.Println(middlewareIndex)
@@ -62,6 +128,28 @@ func WithLog() pkg.ClientOptionFunc {
 	return func(c *pkg.ClientOptions) {
 		c.Middleware = append(c.Middleware, LogHandler{})
 	}
+}
+
+func GetGhbpRules(client *pkg.Client, owner, repo, branch string) (response []byte, err error) {
+	// Since Octokit Go SDK is generated using Kiota, refer to https://learn.microsoft.com/en-us/openapi/kiota/quickstarts/go
+	// GET /repos/{owner}/{repo}/branches/{branch}/protection
+	responseModel, err := client.Repos().ByOwnerId(owner).ByRepoId(repo).Branches().ByBranch(branch).Protection().Get(context.Background(), nil)
+	if err != nil {
+		if apiError, ok := err.(*models.BasicError); ok {
+			statusCode := apiError.ResponseStatusCode
+			errorMessage := apiError.GetMessage()
+			// Branch has no protection rules
+			if statusCode == http.StatusNotFound && *errorMessage == BranchNotProtectedMessage {
+				response = []byte("{}")
+				err = nil
+				return
+			}
+		}
+		return
+	}
+
+	response, err = kiotaSerialization.SerializeToJson(responseModel)
+	return
 }
 
 func main() {
@@ -106,9 +194,35 @@ func main() {
 	response, err := client.Repos().ByOwnerId(ownerId).ByRepoId(repoId).Branches().ByBranch(branchName).Protection().Get(context.Background(), nil)
 
 	if err != nil {
-		log.Fatalf("error getting branch protection rules: %v", err)
+		// Check if the error contains an HTTP response
+		if basicError, ok := err.(*models.BasicError); ok {
+			// Access the response and status code
+			// statusCode := basicError.ResponseStatusCode
+			log.Println(basicError.GetAdditionalData())
+			log.Printf("Message: %s\n", *basicError.GetMessage())                    // Not Found, OR Branch not protected
+			log.Printf("Status: %s\n", *basicError.GetStatus())                      // 404, OR basicError.ResponseStatusCode
+			log.Printf("Documentation URL: %s\n", *basicError.GetDocumentationUrl()) // https://docs.github.com/rest/branches/branch-protection#get-branch-protection
+
+		} else {
+			// Handle non-API error
+			printErrorProperties(err)
+			log.Fatalf("Non-API error: %v", err)
+		}
 	}
+
 	result, _ := kiotaSerialization.SerializeToJson(response)
 
 	log.Println(string(result))
+
+	responseBytes, err := GetGhbpRules(client, ownerId, repoId, branchName)
+	if err != nil {
+		// Check if the error contains an HTTP response
+		if _, ok := err.(*models.BasicError); ok {
+		} else {
+			// Handle non-API error
+			printErrorProperties(err)
+			log.Fatalf("Non-API error: %v", err)
+		}
+	}
+	fmt.Printf("responseBytes: %v\n", string(responseBytes))
 }
